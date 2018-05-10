237443ce249d5191d8038fa678e8f048e84b0f50
Stephen Crecco <crecco61790@yahoo.com>
2018-05-10 08:51:58 -0400

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { TrackerComponent } from './tracker/tracker.component';
-import { HomeComponent } from './home/home.component';
-import { MessagesComponent } from './messages/messages.component';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { LogComponent } from './log/log.component';
 import { ShareComponent } from './share/share.component';
@@ -15,16 +14,23 @@ import { ShareComponent } from './share/share.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TrackerComponent,
     HomeComponent,
-    MessagesComponent,
     NavComponent,
     ProfileComponent,
     LogComponent,
     ShareComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'profile', component: ProfileComponent},
+      { path: 'log', component: LogComponent},
+      { path: 'share', component: ShareComponent},
+      { path: '', redirectTo: '/home', pathMatch: 'full'}
+  ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
