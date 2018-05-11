94f71877d71e391d5f4d867704b9c3cc04eb362c
Stephen Crecco <crecco61790@yahoo.com>
2018-05-11 08:59:38 -0400

    first server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app
+    .use(function(req, res, next){
+        res.write('This is provided by newpaltz.edu\r\n');
+        next();
+    })
+    .get('/hello', function (req, res) {
+        res.write('World');
+        res.end();
+    })
+    .get('/goodbye', function (req, res) {
+        res.write('New Paltz');
+        res.end();
+    })
+    .listen(8080);
